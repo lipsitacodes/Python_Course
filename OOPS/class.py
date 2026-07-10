b80ad092cd5f0bd70b2f3b4bdbb8c033c805b651
Lipsita <lipsitakhadgarai16@gmail.com>
2026-07-10 17:23:44 +0530

    10/07/26 Learned classes and object

---
+++ OOPS/class.py
@@ -0,0 +1,10 @@
+# Class : Class is a blueprint or a template. Eg. From for an Exam that contains name , age , elcetives, father's name etc.
+#Object : Object is a specific instance from the template (class.). Eg Form which contains the data for John Deo , 19 , Kai Deo
+class Emp:
+     company = "Micr"
+     def get_sal(self):
+         return 50000
+e = Emp() # An object of class employee created here
+print(e.get_sal()) #Emp's get_sal method got called here
+e2 = Emp()
+print(e2.get_sal())
